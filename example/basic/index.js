82e497f2bf9dad5452755eb16f644d794ebf08cb
Joseph Luck <joseph.luck@sky.com>
2017-02-23 23:34:23 +0000

    Add component example

--- example/basic/index.js
+++ example/basic/index.js
@@ -1,5 +1,27 @@
 const sakura = require('../../src')
-const h = require('../../src/html').h
+const html = require('../../src/html')
+const h = html.h
+
+class Counter extends html.Component {
+  constructor () {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+  increment () {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  render () {
+    return (
+      <div>
+        <button onclick={() => this.increment()}>{this.state.count} inc</button>
+      </div>
+    )
+  }
+}
 
 function viewOne ({state, prev, methods}) {
   return (
@@ -7,6 +29,7 @@ function viewOne ({state, prev, methods}) {
       <a href="/foo/bar">To bar</a>
       {state.title}
       <input value={state.title} onInput={e => methods.set(e.target.value)} />
+      <Counter />
     </div>
   )
 }
@@ -16,6 +39,7 @@ function viewTwo ({state, prev, methods}) {
       <a href="/foo">To foo</a>
       {state.title}
       <input value={state.title} onInput={e => methods.set(e.target.value)} />
+      <Counter />
     </div>
   )
 }
