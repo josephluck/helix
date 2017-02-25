d6178b36c5457ff220c05a901da88a9d29126c13
Joseph Luck <joseph.luck@sky.com>
2017-02-25 10:55:04 +0000

    Comment types

--- src/types.ts
+++ src/types.ts
@@ -1,31 +1,29 @@
 export namespace Sakura {
+  // Sakura config used during first initialization
   export interface Configuration {
     model: any
     routes: any
   }
 
+  // View (mounted on routes)
   export interface ViewProps {
     state: any
     prev: any
     methods: any
   }
-
   export type View = preact.ComponentConstructor<ViewProps, null>
 
   export type Renderer = (props: any, child: View) => any
 
+  // Routing
   export type RouterRenderer = (url: string) => View
-
   export interface RouterPath {
     pathname: string
   }
-
   export type LocationModelConstructor = (window) => LocationModel
-
   export interface LocationModelReducers {
     set: (state: void, location: string) => any
   }
-
   export interface LocationModel {
     scoped: boolean
     state: any
