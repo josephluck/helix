72c68f772aa7afbf542d323a5629c2f405576e0b
Joseph Luck <joseph.luck@sky.com>
2017-02-25 14:49:14 +0000

    Add dist scripts

---
+++ dist/html.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var preact = require("preact");
+exports.h = preact.h;
+exports.Component = preact.Component;
+exports.default = preact;
