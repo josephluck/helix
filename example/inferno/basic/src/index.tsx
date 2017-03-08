ac8566101e5465a8f5464217c797e8ddce35434e
Joseph Luck <joseph.luck@sky.com>
2017-03-08 21:19:29 +0000

    Add basic example to inferno examples

---
+++ example/inferno/basic/src/index.tsx
@@ -0,0 +1,156 @@
+require('es6-shim')
+import helix from '../../../../src'
+import h, {renderer} from '../../../../src/renderers/inferno'
+
+function links ({
+  onRouteClick,
+}) {
+  return (
+    <div>
+      <a style='margin-right: 10px;' href='/'>view one</a>
+      <a style='margin-right: 10px;' href='/bar'>view two</a>
+      <a style='margin-right: 10px;' href='/bar/123'>view three (123)</a>
+      <a style='margin-right: 10px;' href='/bar/456'>view three (456)</a>
+      <a style='margin-right: 10px;' href='/bar/789'>view three (789)</a>
+      <a style='margin-right: 10px;' onclick={() => onRouteClick('/bar/abc')}>view three (abc)</a>
+      <a style='margin-right: 10px;' onclick={() => onRouteClick('/bar/def')}>view three (def)</a>
+    </div>
+  )
+}
+
+function viewOne (state, prev, actions) {
+  return (
+    <div>
+      {links({
+        onRouteClick: path => actions.location.set(path),
+      })}
+      <h1>view one</h1>
+      {state.title}
+      <div>
+        <input value={state.title} oninput={(e: any) => actions.set(e.target.value)} />
+      </div>
+    </div>
+  )
+}
+function viewTwo (state, prev, actions) {
+  return (
+    <div>
+      {links({
+        onRouteClick: path => actions.location.set(path),
+      })}
+      <h1>view two</h1>
+      {state.title}
+      <div>
+        <input value={state.title} oninput={(e: any) => actions.set(e.target.value)} />
+      </div>
+    </div>
+  )
+}
+function viewThree (state, prev, actions) {
+  return (
+    <div>
+      {links({
+        onRouteClick: path => actions.location.set(path),
+      })}
+      <h1>view three {state.location.params.baz}</h1>
+      {state.title}
+      <div>
+        <input value={state.title} oninput={(e: any) => actions.set(e.target.value)} />
+      </div>
+    </div>
+  )
+}
+
+let mount = document.createElement('div')
+document.body.appendChild(mount)
+
+helix({
+  model: {
+    state: {
+      title: 'not set',
+    },
+    reducers: {
+      set (state, title) {
+        return Object.assign({}, state, {
+          title: title,
+        })
+      },
+    },
+    models: {
+      counter: {
+        scoped: true,
+        state: {
+          count: 1,
+        },
+        reducers: {
+          increment (state, amount) {
+            return {
+              count: state.count + (amount || 1),
+            }
+          },
+        },
+        effects: {
+          incrementAsync (state, actions) {
+            setTimeout(() => {
+              actions.increment(5)
+            }, 1000)
+          },
+        },
+        models: {
+          secondTitle: {
+            scoped: true,
+            state: {
+              title: 'hey',
+            },
+            reducers: {
+              update (state, title) {
+                return {
+                  title: title,
+                }
+              },
+            },
+          },
+        },
+      },
+      foo: {
+        state: {
+          bar: 'baz',
+        },
+      },
+    },
+  },
+  routes: {
+    '': viewOne,
+    'bar': {
+      onEnter (state, prev, actions) {
+        actions.set('You have entered bar')
+        console.log('bar onEnter', state.location.pathname)
+      },
+      onUpdate (state, prev, actions) {
+        actions.set('You have updated bar')
+        console.log('bar onUpdate', state.location.pathname)
+      },
+      onLeave (state, prev, actions) {
+        actions.set('You have left bar')
+        console.log('bar onLeave', state.location.pathname)
+      },
+      view: viewTwo,
+    },
+    'bar/:baz': {
+      onEnter (state, prev, actions) {
+        actions.set(`You have entered bar:/baz ${state.location.params.baz}`)
+        console.log('bar/:baz onEnter', state.location.pathname)
+      },
+      onUpdate (state, prev, actions) {
+        actions.set(`You have updated bar:/baz ${state.location.params.baz}`)
+        console.log('bar/:baz onUpdate', state.location.pathname)
+      },
+      onLeave (state, prev, actions) {
+        actions.set(`You have left bar:/baz ${state.location.params.baz}`)
+        console.log('bar/:baz onLeave', state.location.pathname)
+      },
+      view: viewThree,
+    }
+  },
+  render: renderer(mount),
+})
