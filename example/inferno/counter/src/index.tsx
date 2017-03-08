dae150ae88aa88900cbfe96edc38ce04c156550c
Joseph Luck <joseph.luck@sky.com>
2017-03-08 21:13:49 +0000

    Add basic inferno renderer

--- example/inferno/counter/src/index.tsx
+++ example/inferno/counter/src/index.tsx
@@ -1,66 +1,63 @@
 require('es6-shim')
-import helix from '../../../src'
-import html, {renderer} from '../../../src/renderers/yoyo'
+import helix from '../../../../src'
+import h, {renderer} from '../../../../src/renderers/inferno'
 
 function counterView (state, prev, actions) {
-  return html`
+  return (
     <section class='section hero'>
       <div class='card'>
         <div class='card-content'>
           <div class='title is-2'>
             <span>The count is: </span>
-            <span class=''>${state.count}</span>
+            <span>{state.count}</span>
           </div>
 
-          <hr />
-
           <div class='block control is-grouped'>
-            <a class='control button' onclick=${() => actions.increment(state.amount)}>
+            <a class='control button' onclick={() => actions.increment(state.amount)}>
               Increment
             </a>
-            <a class='control button' onclick=${() => actions.decrement(state.amount)}>
+            <a class='control button' onclick={() => actions.decrement(state.amount)}>
               Decrement
             </a>
           </div>
           <div class='block control is-grouped'>
-            <a class='control button' onclick=${() => actions.incrementAsync(state.amount, state.timeout)}>
-              Increment in ${state.timeout} second${state.timeout > 1 ? 's' : ''}
+            <a class='control button' onclick={() => actions.incrementAsync(state.amount, state.timeout)}>
+              Increment in {state.timeout} second{state.timeout > 1 ? 's' : ''}
             </a>
-            <a class='control button' onclick=${() => actions.decrementAsync(state.amount, state.timeout)}>
-              Decrement in ${state.timeout} second${state.timeout > 1 ? 's' : ''}
+            <a class='control button' onclick={() => actions.decrementAsync(state.amount, state.timeout)}>
+              Decrement in {state.timeout} second{state.timeout > 1 ? 's' : ''}
             </a>
           </div>
-
-          <hr />
           
           <div class='block'>
             <div class='control'>
-              <label class='label'>Adjust increment / decrement amount (${state.amount})</label>
+              <label class='label'>Adjust increment / decrement amount ({state.amount})</label>
               <input
                 type='range'
-                value=${state.amount}
+                value={state.amount}
                 min='1'
                 max='50'
-                oninput=${e => actions.setAmount(parseFloat(e.target.value))}
+                oninput={e => actions.setAmount(parseFloat(e.target.value))}
               />
             </div>
           </div>
+
           <div class='block'>
             <div class='control'>
-              <label class='label'>Adjust async timeout (${state.timeout}s)</label>
+              <label class='label'>Adjust async timeout ({state.timeout}s)</label>
               <input
                 type='range'
-                value=${state.timeout}
+                value={state.timeout}
                 min='1'
                 max='10'
-                oninput=${e => actions.setAsyncTimeout(parseFloat(e.target.value))}
+                oninput={e => actions.setAsyncTimeout(parseFloat(e.target.value))}
               />
             </div>
           </div>
         </div>
       </div>
     </section>
-  `
+  )
 }
 
 let mount = document.createElement('div')
