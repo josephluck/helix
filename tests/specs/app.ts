ea66832f5a518499384188ffe897595789bfb6f0
Joseph Luck <joseph.luck@sky.com>
2017-11-20 11:44:14 +0000

    Convert more tests to nightwatch

--- tests/specs/app.ts
+++ tests/specs/app.ts
@@ -105,6 +105,126 @@ export default {
     browser.end()
   },
 
+  'It doesnt get confused when a query param is after a URL param'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-two-bar-query')
+    browser.getText('#router-params', result => {
+      browser.assert.equal(JSON.parse(result.value).foo, 'bar')
+    })
+    browser.getText('#router-query', result => {
+      browser.assert.equal(JSON.parse(result.value).query, 'wiggle')
+    })
+    browser.end()
+  },
+
+  'It rerenders when query params change'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-one')
+    browser.click('#go-to-page-one-query')
+    browser.expect.element('#number-of-times-rendered')
+      .text.to.equal('3')
+    browser.end()
+  },
+
+  'It rerenders when query params change on the same route'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-five-bar-query')
+    browser.click('#go-to-page-five-bar-query-2')
+    browser.expect.element('#number-of-times-rendered')
+      .text.to.equal('5')
+    // Page 5 has an onUpdate & onLeave hook so +2 counts
+    browser.end()
+  },
+
+  'It calls the onEnter hook when a page is visited'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-three')
+    browser.click('#go-to-page-one')
+    browser.click('#go-to-page-three')
+    browser.expect.element('#on-enter-call-count')
+      .text.to.equal('2')
+    browser.end()
+  },
+
+  'It calls the onLeave hook when a page is left'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-four')
+    browser.click('#go-to-page-one')
+    browser.expect.element('#on-leave-call-count')
+      .text.to.equal('1')
+    browser.end()
+  },
+
+  'It calls the onLeave hook multiple times when the same page is left multiple times'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-four')
+    browser.click('#go-to-page-one')
+    browser.click('#go-to-page-four')
+    browser.click('#go-to-page-one')
+    browser.expect.element('#on-leave-call-count')
+      .text.to.equal('2')
+    browser.end()
+  },
+
+  'It calls the onLeave hook multiple times when switching between two pages that have onLeave'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-four')
+    browser.click('#go-to-page-five-bar')
+    browser.click('#go-to-page-four')
+    browser.click('#go-to-page-five-baz')
+    browser.expect.element('#on-leave-call-count')
+      .text.to.equal('3')
+    browser.end()
+  },
+
+  'It calls the onUpdate hook when the URL parameters change'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-five-bar')
+    browser.click('#go-to-page-five-baz')
+    browser.expect.element('#on-leave-call-count')
+      .text.to.equal('1')
+    browser.end()
+  },
+
+  'It calls the onUpdate hook multiple times when the URL parameters change'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-five-bar')
+    browser.click('#go-to-page-five-baz')
+    browser.click('#go-to-page-five-bar')
+    browser.expect.element('#on-leave-call-count')
+      .text.to.equal('2')
+    browser.end()
+  },
+
+  'It calls the onUpdate hook when the query parameters change'(browser: NightwatchBrowser) {
+    browser
+      .url(server.domain)
+      .waitForElementVisible('body', 2000)
+    browser.click('#go-to-page-five-bar')
+    browser.click('#go-to-page-five-bar-query')
+    browser.expect.element('#on-leave-call-count')
+      .text.to.equal('1')
+    browser.end()
+  },
+
   after(browser, done) {
     server.stop()
     done()
